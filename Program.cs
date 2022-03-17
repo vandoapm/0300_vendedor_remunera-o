cfd8f0661f0c50246fa7d0b93706e39bd3becd13
VANDO MONTEIRO <vando.monteiro@etec.sp.gov.br>
2022-03-17 15:31:20 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0300_vendedor_remuneração
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Exibir o Vendedor, salário fixo e slario + comissão");
+            Console.WriteLine();
+
+            Console.Write("Nome do vendedor: ");
+            string n = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Salário fixo: ");
+            float s = float.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Vendas do mês: ");
+            float v = float.Parse(Console.ReadLine());
+            float r = v * 0.2f;
+
+            Console.WriteLine();
+
+            Console.Write("Vendedor: ");
+            Console.Write(n);
+            Console.Write(" | Salário fixo: ");
+            Console.Write(s);
+            Console.Write(" | Remuneração Total: ");
+            Console.Write(s + r);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("0300_vendedor_remuneração")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("0300_vendedor_remuneração")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("89c02474-f848-4e08-96ee-148a7c392f5c")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
